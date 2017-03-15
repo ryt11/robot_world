c59457765fd542bcef73465e3ce5669c00fe3e50
ryt11 <rileyt_2000@hotmail.com>
2017-03-14 18:26:07 -0600

    add dashboard/avatar extension

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,10 +1,53 @@
 require_relative '../models/robot.rb'
 
+
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/robots' do
+    Robot.database
+    @robots = Robot.all
     erb :index
   end
-end
 
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+     robot = Robot.new(params['robot'])
+     robot.save
+     redirect '/robots'
+  end
+
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit
+
+  end
+
+  get '/robots/:id/delete' do |id|
+    Robot.disintegrate(id.to_i)
+    redirect '/robots'
+  end
+
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
+  end
+
+  get '/dashboard' do
+    @robots = Robot.all
+    @dep_stats = Robot.stats("department")
+    @state_stats = Robot.stats("state")
+    @city_stats = Robot.stats("city")
+    erb :dashboard
+  end
+
+end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,74 @@
 require 'sqlite3'
+require 'pry'
 
 class Robot
+  attr_reader :robot_params, :id
   def initialize(robot_params)
+    @robot_params = robot_params
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+    @id = robot_params["id"] if robot_params["id"]
   end
+
+  def self.stats(section)
+    robots = Robot.all
+    robots.group_by(&section.to_sym)
+  end
+
+
+  def self.find(id)
+    robot = database.execute("SELECT * FROM robots WHERE id= ?;", id).first
+    Robot.new(robot)
+  end
+
+  def self.all
+    robots = database.execute("SELECT * FROM robots;")
+    robots.map { |robot| Robot.new(robot) }
+  end
+
+  def self.disintegrate(id)
+    database.execute("DELETE FROM robots WHERE id = ?;", id)
+  end
+
+  def self.update(id, robot_params)
+      database.execute("UPDATE robots
+                            SET name = ?,
+                                city = ?,
+                                state = ?,
+                                department = ?
+                                WHERE id = ?;",
+                                robot_params[:name],
+                                robot_params[:city],
+                                robot_params[:state],
+                                robot_params[:department],
+                                id
+                            )
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?, ?, ?);", name, city, state, department)
+  end
+
+  def name
+    robot_params['name']
+  end
+
+  def city
+    robot_params['city']
+  end
+
+  def state
+    robot_params['state']
+  end
+
+  def department
+    robot_params['department']
+  end
+
 end
